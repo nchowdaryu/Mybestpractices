d8ccedd43b841d2df5fe4b896167068264caee4d
nchowdaryu <35385729+nchowdaryu@users.noreply.github.com>
2018-02-08 15:00:34 -0500

    Create eMailUtil.java

---
+++ com.utils/eMailUtil.java
@@ -0,0 +1,63 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.TimeZone;
+
+import javax.inject.Inject;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Multipart;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
+
+import org.apache.commons.lang.StringUtils;
+import org.springframework.mail.javamail.JavaMailSender;
+
+//configure SMTP server 
+public class EmailServiceUtil {
+	
+	static final String FROM = "test@test.com";
+	
+	@Inject
+	JavaMailSender mailSender;
+	
+	public void sendEmail(String commaDelimitedEmailAddresses, String subject, String body, File attachment) throws MessagingException, IOException {
+		MimeMessage msgMime = mailSender.createMimeMessage();
+		
+		TimeZone.setDefault(TimeZone.getTimeZone("EST"));
+		
+		List<InternetAddress> emailAddresses = new ArrayList<InternetAddress>();		
+		for(String emailAddress : StringUtils.split(commaDelimitedEmailAddresses, ",")) {
+			emailAddresses.add(new InternetAddress(emailAddress));
+		}
+		msgMime.setRecipients(Message.RecipientType.TO, emailAddresses.toArray(new InternetAddress[emailAddresses.size()]));
+		msgMime.setFrom(new InternetAddress(FROM));			
+		msgMime.setSubject(subject);
+		msgMime.setSentDate(new Date());
+		
+		if(attachment == null) {
+			msgMime.setText(body);
+		}
+		else {
+			Multipart multipart = new MimeMultipart();
+
+			MimeBodyPart textPart = new MimeBodyPart();
+			textPart.setText(body);
+			
+			MimeBodyPart attachmentPart = new MimeBodyPart();
+			attachmentPart.attachFile(attachment);
+			
+			multipart.addBodyPart(textPart);
+			multipart.addBodyPart(attachmentPart);
+			
+			msgMime.setContent(multipart);
+		}
+		
+		mailSender.send(msgMime);
+	}
+
+}
